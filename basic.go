2f97487ad54f50919b2a122448bd292cb8c71123
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2022-07-25 17:35:06 +0800

    Update basic.go
    fix the issue when map is nil
    Also, change the function target to pointer. If it is not pointer, the listener map will always be blank

--- basic.go
+++ basic.go
@@ -22,21 +22,26 @@ type BasicEventTarget struct {
 	listeners map[string][]Listener
 }
 
-func (t BasicEventTarget) AddEventListener(eventName string, listener Listener) {
-	lAdded := reflect.ValueOf(listener)
-	for _, l := range t.listeners[eventName] {
-		l1 := reflect.ValueOf(l)
-
-		if l1.Pointer() == lAdded.Pointer() {
-			return
-		}
+func (t *BasicEventTarget) AddEventListener(eventName string, listener Listener) {
+	if t.listeners == nil {
+		t.listeners = make(map[string][]Listener)
 	}
 
+	lAdded := reflect.ValueOf(listener)
+	if list, has := t.listeners[eventName]; has {
+		for _, l := range list {
+			l1 := reflect.ValueOf(l)
+
+			if l1.Pointer() == lAdded.Pointer() {
+				return
+			}
+		}
+	}
 	t.listeners[eventName] = append(t.listeners[eventName], listener)
 }
 
-func (t BasicEventTarget) RemoveEventListener(eventName string, listener Listener) {
-	if len(t.listeners[eventName]) == 0 {
+func (t *BasicEventTarget) RemoveEventListener(eventName string, listener Listener) {
+	if t.listeners[eventName] == nil || len(t.listeners[eventName]) == 0 {
 		return
 	}
 	lRemoved := reflect.ValueOf(listener)
@@ -51,7 +56,7 @@ func (t BasicEventTarget) RemoveEventListener(eventName string, listener Listene
 	}
 }
 
-func (t BasicEventTarget) DispatchEvent(e NonDispatchedEvent) bool {
+func (t *BasicEventTarget) DispatchEvent(e NonDispatchedEvent) bool {
 	eventName := e.Type()
 
 	event := BasicEvent{
